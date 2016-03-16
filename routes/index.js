e10dfb497a17c57ded35263cd9ec088092f3f56d
Matt Ball <matt.ball@qubitdigital.com>
2016-03-16 17:16:22 +0000

    Updated styles. Basic wrapper around OTD API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var api = require('../lib/on-the-dot');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  api.retrieveBooking('Qubit-01', '17450362', function(data) {
+    console.log(data);
+  });
 });
 
 module.exports = router;
